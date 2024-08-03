1bbbe530c91b20eea44177a33e06a9a0d0ae9a75
Rayane <rayanehiouani.rh@gmail.com>
2024-08-03 19:32:45 +0100

    finished all frontend logic

--- src/app/(user)/checkout/page.tsx
+++ src/app/(user)/checkout/page.tsx
@@ -1,174 +1,42 @@
-'use client'
 import Navbar from "@/app/components/navbar/Navbar";
 import "../../styles/check.css"
-
-import { useEffect, useState } from "react"
-
-
-export default function Checkout({ searchParams }: { searchParams : { source : string } }){
+import CheckPage from "@/app/components/checkPage/CheckPage";
+import { PrismaClient } from '@prisma/client'
 
 
+const prisma = new PrismaClient()
 
 
-    const [articles,setArticles] = useState<any>([]);
-    const [price,setPrice] = useState<any>(null);
+interface Wilaya {
+    id: string,
+    name: string,
+    price: number,
+    hide: boolean
+}[]
+
+async function getWilayas(): Promise<Wilaya[]> {
+     const res = await prisma.wilaya.findMany({});
+
+     if(!res){
+        throw new Error("failed to fetch");
+      }
     
-    useEffect(()=>{
-        if(searchParams.source === "article"){
-        const storedArticle = sessionStorage.getItem('articles');
-        if (storedArticle) {
-          const lcl = JSON.parse(storedArticle);  
-          setArticles(JSON.parse(storedArticle));
-          setPrice(lcl[0].price);
-        }
-    }
-    else if (searchParams.source === "cart"){
-        const storedCart = JSON.parse(localStorage.getItem('cart') || '') as any[];
-        const priceL = JSON.parse(localStorage.getItem('price') || '') as any[];
-        if(storedCart && priceL){
-        setPrice(priceL);    
-        setArticles(storedCart);
-        }
-    }
-    },[])
+      return res;
+}
+
+
+export default async function Checkout({ searchParams }: { searchParams : { source : string } }){
+
+    const wilayas = await getWilayas()
+    .finally(async ()=> {
+        await prisma.$disconnect();
+    });
 
 
     return(
         <div>
            <Navbar />
-           <div className="checkoutDiv">
-        <div className="infosDiv">
-          <div className="personnalInfos">
-            <div className="container">
-              <h1>Shipping</h1>
-              <p>Please enter your shipping details.</p>
-              <hr />
-              <form onSubmit={(e)=>{}} className="form">
-                <div className="fields fields--2">
-                  <label className="field">
-                    <span className="field__label">
-                      Full name
-                    </span>
-                    <input
-                      className="field__input"
-                      type="text"
-                      required
-                      id="firstname"
-                      name="fullName"
-                      onChange={(e)=>{}}
-                    />
-                  </label>
-                  <label className="field">
-                    <span className="field__label">
-                      email
-                    </span>
-                    <input required name="email" className="field__input" type="email" id="lastname"
-                     />
-                  </label>
-                </div>
-                <label className="field">
-                  <span className="field__label">
-                    Phone number
-                  </span>
-                  <input required name="phone" className="field__input" type="text" id="address" 
-                  />
-                </label>
-                <div className="fields fields--2">
-                  <label className="field">
-                    <span className="field__label">
-                      City
-                    </span>
-                    <select required name="city"  className="field__input" id="state">
-                      <option hidden value=""></option>
-                    </select>
-                  </label>
-                  <label className="field">
-                    <span className="field__label">
-                      District
-                    </span>
-                    <select required name="district" className="field__input" id="state">
-                    <option hidden value=""></option>
-                    </select>
-                  </label>
-                </div>
-                <label className="field">
-                  <span className="field__label">
-                    Address
-                  </span>
-                  <input required name="address" className="field__input" type="text" id="lastname"
-                   />
-                </label>
-                <button type="submit" className="btn btn-dark">Submit</button>
-              </form>
-              <hr></hr>
-            </div>
-          </div>
-        </div>
-
-            <div className="layout__sidebar -checkout">
-              <div className="sidebar__content">
-                   <div className="summary">
-                      <div className="summary__header">
-                       <h5 className="summary__title">Order Summary</h5>
-                      </div>
-
-                      <div className="summary__body">
-                         <div className="summary__products">
-                           {articles === null ? "" : 
-                            articles.map((article:any,index:number)=>{
-                                return(
-                                    <div key={index} className="product">
-                               <a className="product__area -image" href={"/articles/" + article.name + "/" + article.id}>
-                                <img className="articleImg" src={article.image} alt={article.name}></img>
-                               </a>
-                               <div className="product__area -content">
-                                   <h4 className="product__title">{article.name}</h4>
-                                   <div className="product__attributes">
-                                     <div style={{display:"flex",flexDirection:"row"}}>
-                                      <span className="product__attrKey">
-                                      Size:
-                                      </span>
-                                      <span className="product__attrValue">
-                                      {article.size}
-                                      </span>
-                                      </div>
-                                      <div style={{display:"flex",flexDirection:"row"}}>
-                                      <span className="product__attrKey">
-                                      Color:
-                                      </span>
-                                      <span className="product__attrValue">
-                                      {article.color}
-                                      </span>
-                                      </div>
-                                   </div>
-                               </div>
-                               <div  className="product__area -prices">
-                               <span className="product__price">
-                               {article.price} DA
-                               </span>
-                               </div>
-                            </div>
-                                )
-                            })
-                           }
-                         </div>
-                      </div>
-                      <div className="summary__footer">
-                         <div className="summaryItem -total">
-                           <div className="summaryItem__title">
-                             TOTAL
-                           </div>
-                           <div className="summaryItem__value" data-qa="totalAmountToBePaid">
-                             {articles === null ? "" : price}
-                               DA
-                           </div>
-                         </div>
-                      </div>
-                   </div>
-              </div>
-
-        </div>
-      </div>
+           <CheckPage source={searchParams.source} wilayas={wilayas} />
         </div>
     )
 }
---
+++ src/app/components/checkPage/CheckPage.tsx
@@ -0,0 +1,304 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react"
+
+
+interface Wilaya {
+  id: string,
+  name: string,
+  price: number,
+  hide: boolean
+}
+
+interface Props {
+  source: string,
+  wilayas: Wilaya[]
+}
+
+function formatDate(date: Date) {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is zero based
+  const year = date.getFullYear();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
+
+export default function CheckPage({ source , wilayas }: Props){
+
+  const router = useRouter();
+
+  const [order,setOrder] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    wilaya: "",
+    district: "",
+    address: "",
+    articles: [],
+    total: "",
+    status: "Waiting"
+  })
+
+
+
+
+
+    const [articles,setArticles] = useState<any>([]);
+    const [price,setPrice] = useState<any>(null);
+    
+    useEffect(()=>{
+        if(source === "article"){
+        const storedArticle = sessionStorage.getItem('article');
+        if (storedArticle) {
+          const lcl = JSON.parse(storedArticle);  
+          setArticles(JSON.parse(storedArticle));
+          setPrice(lcl[0].price);
+          setOrder((prev:any)=>({...prev,total:lcl[0].price,articles:lcl}));
+        }
+    }
+    else if (source === "cart"){
+        const storedCart = JSON.parse(localStorage.getItem('cart') || '') as any[];
+        const priceL = JSON.parse(localStorage.getItem('price') || '') as any[];
+        if(storedCart && priceL){
+        setPrice(priceL);    
+        setArticles(storedCart);
+        setOrder((prev:any)=>({...prev,total:priceL,articles:storedCart}));
+        }
+    }
+    },[])
+
+    const handleSubmit = async () => {
+      const currentDate = new Date();
+      const formattedDate = formatDate(currentDate).toString();
+
+      const formData = new FormData();
+
+      let artcs = "";
+      let clrs = "";
+      let szs = ""
+      if(source === "cart"){
+      order.articles.map((article:any)=>{
+         artcs = artcs + article.name + " ---- "
+         clrs = clrs + article.color + " ---- "
+         szs = szs + article.size + " ---- "
+      })
+    }
+    else if (source === "article"){
+      artcs = articles[0].name;
+      clrs = articles[0].color;
+      szs = articles[0].size;
+    }
+
+        formData.append("FULLNAME", order.fullName);
+        formData.append("PHONE",order.phone);
+        formData.append("WILAYA",order.wilaya);
+        formData.append("DISTRICT",order.district);
+        formData.append("ADDRESS",order.address);
+        formData.append("DATE",formattedDate);
+        formData.append("ARTICLE",artcs);
+        formData.append("SIZE",szs);
+        formData.append("COLOR",clrs);
+        formData.append("TOTAL", order.total);
+        formData.append("STATUS", "Waiting");
+        try {
+          await fetch("https://script.google.com/macros/s/AKfycby35eazAahsGnxYKoLa2Vw0GVjjdw55UtRKd9Ax15WnvMF6_EBEx2tF3g7x70yBtwN9GQ/exec",{
+            method: "POST",
+            mode: "no-cors",
+            body: formData,
+            })
+  
+          router.push("/");
+        } catch (error) {
+          console.log(error);
+        }
+        
+
+      
+
+    }
+
+
+    return(
+           <div className="checkoutDiv">
+        <div className="infosDiv">
+          <div className="personnalInfos">
+            <div className="container">
+              <h1>Shipping</h1>
+              <p>Please enter your shipping details.</p>
+              <hr />
+              <form onSubmit={(e)=>{e.preventDefault();handleSubmit()}} className="form">
+                <div className="fields fields--2">
+                  <label className="field">
+                    <span className="field__label">
+                      Full name
+                    </span>
+                    <input
+                      className="field__input"
+                      type="text"
+                      required
+                      id="firstname"
+                      name="fullName"
+                      onChange={(e)=>setOrder((prev)=>({...prev,[e.target.name]:e.target.value}))}
+                    />
+                  </label>
+                  <label className="field">
+                    <span className="field__label">
+                      email
+                    </span>
+                    <input required name="email" className="field__input" type="email" id="lastname"
+                    onChange={(e)=>setOrder((prev)=>({...prev,[e.target.name]:e.target.value}))}
+                     />
+                  </label>
+                </div>
+                <label className="field">
+                  <span className="field__label">
+                    Phone number
+                  </span>
+                  <input required name="phone" className="field__input" type="text" id="address" 
+                  onChange={(e)=>setOrder((prev)=>({...prev,[e.target.name]:e.target.value}))}
+                  />
+                </label>
+                <div className="fields fields--2">
+                  <label className="field">
+                    <span className="field__label">
+                      Wilaya
+                    </span>
+                    <select required name="city"  className="field__input" id="state"
+                    onChange={(e) => {
+                    const selectedWilaya = wilayas.find(w => w.name === e.target.value);
+                    setOrder((prev) => ({
+                      ...prev,
+                      wilaya: e.target.value,
+                      total: prev.total + (selectedWilaya ? selectedWilaya.price : 0),
+                    }));
+                  }}
+                    >
+                      <option hidden value=""></option>
+                      <option value="Alger">Alger</option>
+                      {wilayas.map((wilaya,key)=>{
+                        return(
+                          <option key={key} value={wilaya.name}>{wilaya.name}</option>
+                        )
+                      })}
+                    </select>
+                  </label>
+                  <label className="field">
+                    <span className="field__label">
+                      District
+                    </span>
+                    <input required name="district" className="field__input" type="text" id="district" 
+                  onChange={(e)=>setOrder((prev)=>({...prev,[e.target.name]:e.target.value}))}
+                  />
+                  </label>
+                </div>
+                <label className="field">
+                  <span className="field__label">
+                    Address
+                  </span>
+                  <input required name="address" className="field__input" type="text" id="lastname"
+                   onChange={(e)=>setOrder((prev)=>({...prev,[e.target.name]:e.target.value}))}
+                   />
+                </label>
+                <button type="submit" className="btn btn-dark">Submit</button>
+              </form>
+              <hr></hr>
+            </div>
+          </div>
+        </div>
+
+            <div className="layout__sidebar -checkout">
+              <div className="sidebar__content">
+                   <div className="summary">
+                      <div className="summary__header">
+                       <h5 className="summary__title">Order Summary</h5>
+                      </div>
+
+                      <div className="summary__body">
+                         <div className="summary__products">
+                           {articles === null ? "" : 
+                            articles.map((article:any,index:number)=>{
+                                return(
+                                    <div key={index} className="product">
+                               <a className="product__area -image" href={"/articles/" + article.name + "/" + article.id}>
+                                <img className="articleImg" src={article.image} alt={article.name}></img>
+                               </a>
+                               <div className="product__area -content">
+                                   <h4 className="product__title">{article.name}</h4>
+                                   <div className="product__attributes">
+                                     <div style={{display:"flex",flexDirection:"row"}}>
+                                      <span className="product__attrKey">
+                                      Size:
+                                      </span>
+                                      <span className="product__attrValue">
+                                      {article.size}
+                                      </span>
+                                      </div>
+                                      <div style={{display:"flex",flexDirection:"row"}}>
+                                      <span className="product__attrKey">
+                                      Color:
+                                      </span>
+                                      <span className="product__attrValue">
+                                      {article.color}
+                                      </span>
+                                      </div>
+                                   </div>
+                               </div>
+                               <div  className="product__area -prices">
+                               <span className="product__price">
+                               {article.price} DA
+                               </span>
+                               </div>
+                            </div>
+                                )
+                            })
+                           }
+                         </div>
+                      </div>
+                      <div className="summary__footer">
+                         <div className="summaryItem -total">
+                           <div className="summaryItem__title">
+                             TOTAL PRICE
+                           </div>
+                           <div className="summaryItem__value" data-qa="totalAmountToBePaid">
+                             {articles === null ? "" : price}
+                               DA
+                           </div>
+                         </div>
+                         <div className="d-flex justify-content-end pe-4">
+                            +
+                         </div>
+                         <div className="summaryItem -total">
+                         <div className="summaryItem__title">
+                             DELIVERY
+                           </div>
+                           <div className="summaryItem__value" data-qa="totalAmountToBePaid">
+                             {order.wilaya === "" ? "" : Number(order.total) - price}
+                               DA
+                           </div>
+                         </div>
+                         <div className="d-flex justify-content-end pe-4">
+                            =
+                         </div>
+                         <div className="summaryItem -total">
+                         <div className="summaryItem__title">
+                             TOTAL
+                           </div>
+                           <div className="summaryItem__value" data-qa="totalAmountToBePaid">
+                             {order.wilaya === "" ? price : order.total}
+                               DA
+                           </div>
+                         </div>
+                      </div>
+                   </div>
+              </div>
+
+        </div>
+      </div>
+    )
+}
+
+
